7e3d452d684f457186818e8cd88ec766ef1541b3
Tanel Alumae <tanel@juur.grid.eenet.ee>
2014-08-27 13:26:09 +0300

    updated results

--- run_tedlium.sh
+++ run_tedlium.sh
@@ -39,7 +39,7 @@ fi
 
 if [ $stage -le 1 ]; then
   # Train a duration model based on alignments in exp/tri3_mmi_b0.1_ali
-  ./dur-model/train_dur_model.sh --nj $nj --cmd "$train_cmd" --cuda-cmd "$cuda_cmd" --pylearn-dir $pylearn_dir --aggregate-data-args="$aggregate_data_args" \
+  ./dur-model/train_dur_model.sh --nj $nj --cmd "$train_cmd" --cuda-cmd "$cuda_cmd" --pylearn-dir $pylearn_dir --aggregate-data-args "$aggregate_data_args" \
     --stage 0 \
     --language ENGLISH \
     --stress-dict dur-model/python/lat-model/data/en/cmudict.0.7a.lc \
@@ -85,12 +85,13 @@ fi
 #%WER 27.4 | 507 17792 | 78.3 16.6 5.1 5.7 27.4 96.4 | -0.145 | exp/tri3/decode_dev/score_16/ctm.filt.filt.sys
 #%WER 32.0 | 507 17792 | 74.6 20.0 5.4 6.6 32.0 97.4 | -0.229 | exp/tri3/decode_dev.si/score_13/ctm.filt.filt.sys
 #%WER 23.9 | 507 17792 | 81.2 14.8 4.0 5.0 23.9 93.9 | -0.108 | exp/tri3_mmi_b0.1/decode_dev_it4/score_13/ctm.filt.filt.sys
-#%WER 23.3 | 507 17792 | 81.8 14.4 3.8 5.1 23.3 93.9 | -0.132 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.15/score_14/ctm.filt.filt.sys
-#%WER 23.3 | 507 17792 | 81.7 14.3 3.9 5.1 23.3 93.9 | -0.127 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.18/score_14/ctm.filt.filt.sys
-#%WER 23.3 | 507 17792 | 81.7 14.4 3.9 5.0 23.3 93.9 | -0.132 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.20/score_14/ctm.filt.filt.sys
-#%WER 23.2 | 507 17792 | 81.8 14.4 3.7 5.1 23.2 93.7 | -0.154 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.15/score_15/ctm.filt.filt.sys
-#%WER 23.3 | 507 17792 | 81.9 14.4 3.7 5.2 23.3 93.7 | -0.154 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.18/score_14/ctm.filt.filt.sys
-#%WER 23.2 | 507 17792 | 81.8 14.3 3.9 5.0 23.2 93.7 | -0.155 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.20/score_15/ctm.filt.filt.sys
+#%WER 23.3 | 507 17792 | 82.0 14.5 3.5 5.3 23.3 93.7 | -0.136 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.15/score_13/ctm.filt.filt.sys
+#%WER 23.3 | 507 17792 | 82.0 14.4 3.6 5.3 23.3 93.7 | -0.136 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.18/score_13/ctm.filt.filt.sys
+#%WER 23.3 | 507 17792 | 81.9 14.4 3.7 5.2 23.3 93.7 | -0.137 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.2_p0.20/score_13/ctm.filt.filt.sys
+#%WER 23.2 | 507 17792 | 81.8 14.6 3.6 5.0 23.2 93.7 | -0.141 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.15/score_15/ctm.filt.filt.sys
+#%WER 23.2 | 507 17792 | 82.1 14.4 3.5 5.4 23.2 93.7 | -0.167 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.18/score_13/ctm.filt.filt.sys
+#%WER 23.2 | 507 17792 | 81.9 14.4 3.7 5.1 23.2 93.5 | -0.141 | exp/dur_model_tri3_mmi_b0.1/decode_dev_it4_s0.3_p0.20/score_14/ctm.filt.filt.sys
+
 
 #TEST SPEAKERS
 
@@ -99,10 +100,9 @@ fi
 #%WER 24.8 | 1155 27512 | 79.4 16.4 4.2 4.2 24.8 93.5 | -0.054 | exp/tri3/decode_test/score_16/ctm.filt.filt.sys
 #%WER 29.9 | 1155 27512 | 74.6 20.3 5.2 4.5 29.9 95.2 | -0.115 | exp/tri3/decode_test.si/score_16/ctm.filt.filt.sys
 #%WER 21.3 | 1155 27512 | 82.4 14.0 3.6 3.8 21.3 92.4 | 0.045 | exp/tri3_mmi_b0.1/decode_test_it4/score_13/ctm.filt.filt.sys
-#%WER 20.8 | 1155 27512 | 82.7 13.8 3.5 3.6 20.8 91.5 | 0.028 | exp/dur_model_tri3/decode_test_it4_s0.2_p0.18/score_15/ctm.filt.filt.sys
-#%WER 20.8 | 1155 27512 | 83.0 13.7 3.3 3.8 20.8 91.4 | 0.029 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.15/score_14/ctm.filt.filt.sys
-#%WER 20.8 | 1155 27512 | 82.9 13.7 3.4 3.7 20.8 91.6 | 0.024 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.18/score_14/ctm.filt.filt.sys
-#%WER 20.8 | 1155 27512 | 82.9 13.7 3.4 3.7 20.8 91.5 | 0.024 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.20/score_14/ctm.filt.filt.sys
-#%WER 20.8 | 1155 27512 | 83.1 13.8 3.1 4.0 20.8 90.6 | 0.008 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.15/score_14/ctm.filt.filt.sys
-#%WER 20.9 | 1155 27512 | 83.0 13.8 3.2 3.9 20.9 90.9 | 0.006 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.18/score_14/ctm.filt.filt.sys
-#%WER 20.9 | 1155 27512 | 83.0 13.8 3.3 3.9 20.9 91.0 | 0.007 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.20/score_14/ctm.filt.filt.sys
+#%WER 20.8 | 1155 27512 | 83.0 13.7 3.3 3.8 20.8 91.8 | 0.024 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.15/score_14/ctm.filt.filt.sys
+#%WER 20.8 | 1155 27512 | 83.0 13.6 3.4 3.8 20.8 91.7 | 0.025 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.18/score_14/ctm.filt.filt.sys
+#%WER 20.8 | 1155 27512 | 83.0 13.6 3.4 3.7 20.8 91.8 | 0.021 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.2_p0.20/score_14/ctm.filt.filt.sys
+#%WER 20.7 | 1155 27512 | 83.2 13.7 3.0 3.9 20.7 91.0 | 0.003 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.15/score_14/ctm.filt.filt.sys
+#%WER 20.7 | 1155 27512 | 83.1 13.7 3.2 3.8 20.7 91.3 | 0.006 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.18/score_14/ctm.filt.filt.sys
+#%WER 20.8 | 1155 27512 | 83.2 13.8 3.0 4.0 20.8 91.3 | -0.012 | exp/dur_model_tri3_mmi_b0.1/decode_test_it4_s0.3_p0.20/score_13/ctm.filt.filt.sys
